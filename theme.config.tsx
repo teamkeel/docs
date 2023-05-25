bcc2012676d3fb426e4274a4a29f0b3e6fc2e025
Jon Bretman <jon.bretman@gmail.com>
2023-05-25 15:59:00 +0100

    Initial commit after forking

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,15 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Keel</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/teamkeel/keel",
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/teamkeel/keel-docs",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Keel",
   },
-}
+};
 
-export default config
+export default config;
