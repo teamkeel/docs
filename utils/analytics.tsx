962c3421bbaddb017577d5594f0b78ada660c921
tomfrew <tom@keel.xyz>
2024-03-21 18:22:59 +0000

    analytics

--- utils/analytics.tsx
+++ utils/analytics.tsx
@@ -1,4 +1,5 @@
 import Router from "next/router";
+import Script from "next/script";
 import { useEffect } from "react";
 
 declare global {
@@ -30,7 +31,16 @@ export const Analytics = () => {
     };
   }, []);
 
-  return null;
+  if (!process.env.NEXT_PUBLIC_KOALA_PK) return null;
+
+  return (
+    <Script
+      id="koala-snippet"
+      dangerouslySetInnerHTML={{
+        __html: `!function(t){if(window.ko)return;window.ko=[],["identify","track","removeListeners","open","on","off","qualify","ready"].forEach(function(t){ko[t]=function(){var n=[].slice.call(arguments);return n.unshift(t),ko.push(n),ko}});var n=document.createElement("script");n.async=!0,n.setAttribute("src","https://cdn.getkoala.com/v1/${process.env.NEXT_PUBLIC_KOALA_PK}/sdk.js"),(document.body || document.head).appendChild(n)}();`,
+      }}
+    />
+  );
 };
 
 function trackPage() {
